4c4cce6b19d7255efc9da6d5129310fd873c10cc
mif04953 <mif04953@lxibm007.mif>
2022-11-28 09:52:55 +0200

    slurm setup

--- example_run.sh
+++ example_run.sh
@@ -1,1 +1,3 @@
-nice -n 19 snakemake --use-conda --conda-frontend mamba --configfile configs/test.yaml -c 80 --use-singularity -f  rosetta_static_evaluation
+snakemake  --profile simple  --configfile configs/test.yaml -k  test_run/pdb_proc/process/3BZD_seqresMatched_promod.pdb
+snakemake  --profile simple  --configfile configs/test.yaml -f   test_run/pdb_proc/process/3BZD_seqresMatched_promod.pdb
+#nice -n 19 snakemake --use-conda --conda-frontend mamba --configfile configs/test.yaml -c 80 --use-singularity -f  rosetta_static_evaluation
